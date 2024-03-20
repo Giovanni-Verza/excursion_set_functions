dbaf580573087d4fd17e9a9e85343d24b9992134
gverza <giovanni.verza@pd.infn.it>
2024-03-20 09:47:22 -0400

    submodule cpp structure

--- src/cpp/excursion_set_functions.cpp
+++ src/cpp/excursion_set_functions.cpp
@@ -3,8 +3,10 @@
 
 namespace py = pybind11;
 
-
+//namespace module_a {
 void init_ex_set_analytical(py::module_ &m);
+
+//}
 
 void init_spline(py::module_ &m);
 
@@ -14,10 +16,26 @@ void init_ex_set_cholenski_corr(py::module_ &m);
 
 void init_ex_set_integration(py::module_ &m);
 
+
 PYBIND11_MODULE(excursion_set_functions, m) {
-    init_ex_set_analytical(m);
-    init_spline(m);
-    init_ex_set_cholenski(m);
-    init_ex_set_cholenski_corr(m);
-    init_ex_set_integration(m);
+    //init_ex_set_analytical(m);
+    //auto m_a = m.def_submodule("module_analytical", "This is A.");
+    //m_a.def("analytical", &module_analytical::init_ex_set_analytical);
+    //py::module analytical = m.def_submodule("analytical", "...");
+    //analytical.def("analytical", &module_analytical::init_ex_set_analytical, "Do some work");
+    auto m_analytical = m.def_submodule("analytical", "Analytical multiplicity functions");
+    init_ex_set_analytical(m_analytical);
+    //m_a.def("func", &module_a::init_ex_set_analytical);
+    auto m_spline = m.def_submodule("spline", "Spline functions");
+    init_spline(m_spline);
+
+    auto m_integration = m.def_submodule("integration", "Integration functions");
+    init_ex_set_cholenski(m_integration);
+    init_ex_set_cholenski_corr(m_integration);
+    init_ex_set_integration(m_integration);
 }
+
+//PYBIND11_MODULE(analytical, m) {
+//    m.attr("__name__") = "excursion_set_functions.analytical";
+//    init_ex_set_analytical(m);
+//}
