9167bb6ec50e7536bff7605ed72716d43e577a3e
Giovanni-Verza <giova.verza@gmail.com>
2024-09-23 22:49:13 -0400

    pure python submodule implemented

--- src/excursion_set_functions/__init__.py
+++ src/excursion_set_functions/__init__.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
-import numpy
-
+#
+# import numpy
 from ._core import get_max_threads, set_num_threads, omp_get_num_threads, numerical, spline, integration, analytical #, __doc__, __version__
 
-#from . import excursion_set_python
+from . import excursion_set_python
 
 __all__ = [#"__doc__", "__version__", 
-           "get_max_threads", "set_num_threads", "omp_get_num_threads", "numerical", "spline", "integration", "analytical"
-           #"analytical", "numerical", "spline", "integration"
+           "get_max_threads", "set_num_threads", "omp_get_num_threads", "numerical", "spline", "integration", "analytical",
+           "excursion_set_python"
            ]
---
+++ src/excursion_set_functions/excursion_set_python/__init__.py
@@ -0,0 +1,3 @@
+from . import excursion_set_numba, Integration_functions, multiplicity_functions_analytical, Spline_functions
+
+__all__ = ["excursion_set_numba", "Integration_functions", "multiplicity_functions_analytical", "Spline_functions"]
